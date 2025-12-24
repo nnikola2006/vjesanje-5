c4998d721599e11056b625077ddbbea82ba36060
Dzonex <89349082+nnikola2006@users.noreply.github.com>
2025-12-24 21:41:13 +0100

    Popravka za komentar
    Updated comments for dynamic memory allocation examples.

--- zad3.c
+++ zad3.c
@@ -77,8 +77,23 @@ void ispisi(DOKUMENT d)
 
 // U slucaju alokacije dinamicke memorije ide
 /* int *niz, n = 10;
- * niz = (int*)malloc(n * sizeof(int)); 
- * Ovo alocira 10 elemenata u  nizu
+ * niz = (int*)malloc(n * sizeof(int)); Ovo alocira 10 elemenata u nizu
+ * free(niz);
+ */
+// AKO JE 2D niz onda ide
+/*  int n = 10, **mat = (int **)calloc(n, sizeof(int *));
+    for (i = 0; i < n; i++)
+    {
+        mat[i] = (int *)calloc(m, sizeof(int));
+        // *(mat + i) = (int *)calloc(m, sizeof(int));
+        for (j = 0; j < m; j++, b++)
+            mat[i][j] = b; // *(*(mat + i) + j) = b;
+    }
+
+    // Ovo se mora na kraju programa napisat
+    for (i = 0; i < n; i++)
+        free(mat[i]); // free(*(mat + i));
+    free(mat);
  */
 // MODIFIKACIJE
 void sortiraj(DOKUMENT *d, int n)
